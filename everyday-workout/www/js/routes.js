2862100206e11c932164f408d147b59eece712ad
chang47 <coralbue@gmail.com>
2016-01-02 23:52:41 -0800

    added inital gui

---
+++ everyday-workout/www/js/routes.js
@@ -0,0 +1,53 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+    .state('workoutMainMenu', {
+      url: '/main',
+      templateUrl: 'templates/workoutMainMenu.html',
+      controller: 'workoutMainMenuCtrl'
+    })
+           
+    .state('workoutCalendar', {
+      url: '/calendar',
+      templateUrl: 'templates/workoutCalendar.html',
+      controller: 'workoutCalendarCtrl'
+    })
+  
+    .state('workoutTimeEditor', {
+      url: '/timeEditor',
+      templateUrl: 'templates/workoutTimeEditor.html',
+      controller: 'workoutTimeEditorCtrl'
+    })
+   
+    .state('workoutGlossary', {
+      url: '/glossary',
+      templateUrl: 'templates/workoutGlossary.html',
+      controller: 'workoutGlossaryCtrl'
+    })
+    // not reached
+    .state('workoutExercise', {
+      url: '/exercise',
+      templateUrl: 'templates/workoutExercise.html',
+      controller: 'workoutExerciseCtrl'
+    })
+    // not reached normally, probably shouldn't even exist, just an ad popup
+    .state('workoutAds', {
+      url: '/ads',
+      templateUrl: 'templates/workoutAds.html',
+      controller: 'workoutAdsCtrl'
+    })
+        
+      
+    ;
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/page6');
+
+});
