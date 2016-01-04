fff858e5e5d1af8f96be471347e907c2af7c7ae5
chang47 <coralbue@gmail.com>
2016-01-03 21:43:39 -0800

    fixed page

--- everyday-workout/www/js/routes.js
+++ everyday-workout/www/js/routes.js
@@ -48,6 +48,6 @@ angular.module('app.routes', [])
     ;
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/page6');
+  $urlRouterProvider.otherwise('/main');
 
 });
