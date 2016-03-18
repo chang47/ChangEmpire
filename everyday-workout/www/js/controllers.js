4a841e3532f9c538369dd91831eb6ea008d3c075
Justin <ginka13@gmail.com>
2016-03-17 19:04:40 -0700

    Imported factory from exercise, added Node Server interactions, and created factory that saves data

--- everyday-workout/www/js/controllers.js
+++ everyday-workout/www/js/controllers.js
@@ -1,7 +1,19 @@
 angular.module('app.controllers', [])
 
-.controller('workoutMainMenuCtrl', function($scope) {
-
+.controller('workoutMainMenuCtrl', function($scope,$http) {
+	$scope.request = function() {
+			$http({
+				method: 'POST',
+				url: 'http://localhost:3000/register',
+				data: { platform: 'android', token: "11111111"}
+		}).then(function successCallback(response){
+			console.log("success")
+			console.log(response)
+		}, function errorCallback(response) {
+			console.log("failed")
+			console.log(response)
+		});
+	}
 })
    
 .controller('workoutCalendarCtrl', function($scope, datesFactory) {
@@ -42,19 +54,22 @@ angular.module('app.controllers', [])
 	console.log($stateParams);
 })
    
-.controller('workoutExerciseCtrl', function($scope, $interval) {
+.controller('workoutExerciseCtrl', function($scope, $interval,exerciseFactory) {
 	// Note to self: import a number in to the label.
-	var amountTimes = 2;
+	$scope.groups = exerciseFactory;
+	var exNum = Math.random()*($scope.groups.length);
+	console.log("Random Number:",exNum);
+	var amountTimes = ($scope.groups[0].sets*20)+($scope.groups[0].sets*$scope.groups[0].reps*5);
 	
 	$scope.roundProgressData = {
-		label: 2,
+		label: amountTimes,
 		percentage: 0
 	}
 	
 	
 	$scope.timeLoop = function(){
 		if ($scope.roundProgressData.percentage <= 1){
-			$scope.roundProgressData.percentage += .05;
+			$scope.roundProgressData.percentage += .035;
 		}
 		else {
 			$scope.roundProgressData.percentage = 0;
@@ -80,9 +95,12 @@ angular.module('app.controllers', [])
 
 })
 
-.controller('workoutGlossaryCtrl', function($scope,exerciseFactory) {
+.controller('workoutGlossaryCtrl', function($scope,$location,exerciseFactory) {
 	$scope.disabled = true;
 	$scope.groups = exerciseFactory;
+	groups = JSON.parse(JSON.stringify($scope.groups));
+	console.log("Groups: ",groups);
+	//groups = $scope.groups;
   //for (var i=0; i<10; i++) {
     //for (var j=0; j<3; j++) {
     //}
@@ -111,13 +129,25 @@ angular.module('app.controllers', [])
     $scope.save = function(){
     	console.log("Saved Changes: ",$scope.groups);
     	for(var numOfGroups=0;numOfGroups<$scope.groups.length;numOfGroups++){
-    		if ($scope.groups[numOfGroups].sets){
-    			$scope.groups.sets = 0;
+
+    		if (isNaN(parseInt($scope.groups[numOfGroups].sets))){
+    			$scope.groups[numOfGroups].sets = 0;
     		}
-    		if ($scope.groups[numOfGroups].reps){
-    			$scope.groups.reps = 0;
+    		if (isNaN(parseInt($scope.groups[numOfGroups].reps))){
+    			$scope.groups[numOfGroups].reps = 0;
     		}
+    		$scope.groups[numOfGroups].sets = parseInt($scope.groups[numOfGroups].sets);
+    		$scope.groups[numOfGroups].reps = parseInt($scope.groups[numOfGroups].reps);
     	}
 	    	exerciseFactory.save($scope.groups);
+			groups = JSON.parse(JSON.stringify($scope.groups));
+			$scope.groups = JSON.parse(JSON.stringify(groups));
+			$scope.disabled = true;
+	    	$location.path('#/main');
+    }
+    $scope.cancel = function(){
+			$scope.groups = JSON.parse(JSON.stringify(groups));
+			$scope.disabled = true;
+	    	$location.path('#/main');
     }
 });
